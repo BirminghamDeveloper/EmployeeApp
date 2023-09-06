5f807e3e1d11f9e72137c4a3d0dc9e2ac404a2c6
Mustafa.Sharif <smart1hashi@gmail.com>
2023-09-06 19:02:22 +0100

    Added Extra

--- app/src/main/java/com/hashinology/employeeapp/repo/EmployeeRepository.kt
+++ app/src/main/java/com/hashinology/employeeapp/repo/EmployeeRepository.kt
@@ -3,5 +3,6 @@ package com.hashinology.employeeapp.repo
 import com.hashinology.employeeapp.di.ApiService
 
 class EmployeeRepository(val apiService: ApiService) {
+    // added getAllData function
     suspend fun getAllData() = apiService.getAllInfo()
 }
